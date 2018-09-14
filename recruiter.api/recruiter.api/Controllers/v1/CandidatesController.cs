fad948aec5640f8de85c19bcefb4c15d42121a92
Rich Eodice <rich@eodice.com>
2018-09-14 08:54:06 -0400

    SQL EF Working

---
+++ recruiter.api/recruiter.api/Controllers/v1/CandidatesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+[ApiController]
+[Produces("application/json")]
+public class CandidatesController : ControllerBase
+{
+    private readonly RecruiterSqlContext _repository;
+
+    public CandidatesController(RecruiterSqlContext repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    [Route("api/v1/[controller]/create")]
+    public ActionResult CreateCandidate()
+    {
+        var candidate = new Candidate
+        {
+            Id = Guid.NewGuid(),
+            FirstName = "Richard",
+            MiddleName = "T.",
+            LastName = "Eodice",
+            Email = "reodice@lsnj.org",
+            Phone = "7329215276",
+            Salary = "Make me an offer I can't refuse...",
+            Notes = "Just an average guy who does God-like things.",
+            Available = true,
+            Created = DateTime.Now
+        };
+
+        try
+        {
+            _repository.Candidates.Add(candidate);
+
+            _repository.SaveChanges();
+
+            return Created("Created Candidate: ", candidate.Id);
+        }
+        catch(Exception e)
+        {
+            return BadRequest(e);
+        }
+
+    }
+
+    [HttpGet]
+    [Route("api/v1/[controller]")]
+    public ActionResult Get(int take = 25, int skip = 0)
+    {
+        var query = _repository.Candidates
+            .Take(take)
+            .Skip(skip);
+
+        var results = query.ToList();
+
+        return Ok(results);
+    }
+}
--- recruiter.api/recruiter.api/Startup.cs
+++ recruiter.api/recruiter.api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,6 +26,10 @@ public class Startup
     {
         services.AddCors();
         services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+        var connection = @"Server=192.168.50.116;Database=recruiter;User Id=recruiter;Password=recruiter;";
+        services.AddDbContext<RecruiterSqlContext>
+            (options => options.UseSqlServer(connection));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
