68671bc1573f01e1dcab0349e56fa98e3cc66d6f
Rich Eodice <rich@eodice.com>
2018-09-14 11:46:51 -0400

    Added 2 v1 controllers QuestionsController.cs and ReviewersController.cs.

---
+++ recruiter.api/recruiter.api/Controllers/v1/QuestionsController.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+[ApiController]
+[Produces("application/json")]
+public class QuestionsController : ControllerBase
+{
+    private readonly RecruiterSqlContext _repository;
+
+    public QuestionsController(RecruiterSqlContext repository)
+    {
+        _repository = repository;
+    }
+
+    // Get all Questions, supports paging
+    [HttpGet]
+    [Route("api/v1/[controller]")]
+    public ActionResult GetAll(int take = 25, int skip = 0)
+    {
+        var query = _repository.Questions
+            .Take(take)
+            .Skip(skip);
+
+        var results = query.ToList();
+
+        return Ok(results);
+    }
+
+    // Get one Question from id
+    [HttpGet]
+    [Route("api/v1/[controller]/{id}")]
+    public ActionResult GetOne(Guid id)
+    {
+        var result = _repository.Questions
+            .Find(id);
+
+        if (result != null)
+            return Ok(result);
+        else
+            return NotFound("Question does not exist!");
+    }
+
+    // Get Questions for position
+    [HttpGet]
+    [Route("api/v1/[controller]/position/{positionId}")]
+    public ActionResult GetPositionQuestions(Guid positionId)
+    {
+        var query = _repository.Questions
+            .Where(q => q.PositionId == positionId);
+
+        var results = query.ToList();
+
+        return Ok(results);
+    }
+
+    // Create a Question
+    [HttpPost]
+    [Route("api/v1/[controller]")]
+    public ActionResult Create([FromBody] Question question)
+    {
+        var result = _repository.Questions
+            .Find(question.Id);
+
+        if (result == null)
+        {
+            _repository.Add(question);
+            _repository.SaveChanges();
+            return Ok(question.Id);
+        }
+        else
+        {
+            return BadRequest("Question already exists");
+        }
+    }
+
+    // Update a Question
+    [HttpPut]
+    [Route("api/v1/[controller]")]
+    public ActionResult Update([FromBody] Question question)
+    {
+        if (_repository.Questions.Contains(question))
+        {
+            question.Updated = DateTime.Now;
+            _repository.Update(question);
+            _repository.SaveChanges();
+            return Ok(question.Id);
+        }
+        else
+        {
+            return BadRequest("Question does not exist");
+        }
+    }
+
+    // Delete a Question from id
+    [HttpDelete]
+    [Route("api/v1/[controller]/{id}")]
+    public ActionResult Delete(Guid id)
+    {
+        var result = _repository.Questions
+            .Find(id);
+
+        if (result != null)
+        {
+            _repository.Remove(result);
+            _repository.SaveChanges();
+            return Ok(result.Id);
+        }
+        else
+        {
+            return NotFound("Question does not exist!");
+        }
+    }
+
+    // TESTING
+    [HttpGet]
+    [Route("api/v1/[controller]/create")]
+    public ActionResult CreateQuestion()
+    {
+        var question = new Question
+        {
+            Id = Guid.NewGuid(),
+            PositionId = Guid.Parse("fca8ecef-89ed-4cb0-8084-b5aae15b30c4"),
+            Text = "Network Administration Experience",
+            Active = true,
+            Created = DateTime.Now,
+            Updated = null
+        };
+
+        try
+        {
+            _repository.Questions.Add(question);
+
+            _repository.SaveChanges();
+
+            return Created("Created Question: ", question.Id);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
+
+    }
+}
---
+++ recruiter.api/recruiter.api/Controllers/v1/ReviewersController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+[ApiController]
+[Produces("application/json")]
+public class ReviewersController : ControllerBase
+{
+    private readonly RecruiterSqlContext _repository;
+
+    public ReviewersController(RecruiterSqlContext repository)
+    {
+        _repository = repository;
+    }
+
+    // Get all Reviewers, supports paging
+    [HttpGet]
+    [Route("api/v1/[controller]")]
+    public ActionResult GetAll(int take = 25, int skip = 0)
+    {
+        var query = _repository.Reviewers
+            .Take(take)
+            .Skip(skip);
+
+        var results = query.ToList();
+
+        return Ok(results);
+    }
+
+    // Get one Reviewer from id
+    [HttpGet]
+    [Route("api/v1/[controller]/{id}")]
+    public ActionResult GetOne(Guid id)
+    {
+        var result = _repository.Reviewers
+            .Find(id);
+
+        if (result != null)
+            return Ok(result);
+        else
+            return NotFound("Reviewer does not exist!");
+    }
+
+
+    // Create a Reviewer
+    [HttpPost]
+    [Route("api/v1/[controller]")]
+    public ActionResult Create([FromBody] Reviewer reviewer)
+    {
+        var result = _repository.Reviewers
+            .Find(reviewer.Id);
+
+        if (result == null)
+        {
+            _repository.Add(reviewer);
+            _repository.SaveChanges();
+            return Ok(reviewer.Id);
+        }
+        else
+        {
+            return BadRequest("Reviewer already exists");
+        }
+    }
+
+    // Update a Reviewer
+    [HttpPut]
+    [Route("api/v1/[controller]")]
+    public ActionResult Update([FromBody] Reviewer reviewer)
+    {
+        if (_repository.Reviewers.Contains(reviewer))
+        {
+            _repository.Update(reviewer);
+            _repository.SaveChanges();
+            return Ok(reviewer.Id);
+        }
+        else
+        {
+            return BadRequest("Reviewer does not exist");
+        }
+    }
+
+    // Delete a Reviewer from id
+    [HttpDelete]
+    [Route("api/v1/[controller]/{id}")]
+    public ActionResult Delete(Guid id)
+    {
+        var result = _repository.Reviewers
+            .Find(id);
+
+        if (result != null)
+        {
+            _repository.Remove(result);
+            _repository.SaveChanges();
+            return Ok(result.Id);
+        }
+        else
+        {
+            return NotFound("Reviewer does not exist!");
+        }
+    }
+
+    // TESTING
+    [HttpGet]
+    [Route("api/v1/[controller]/create")]
+    public ActionResult CreateReviewer()
+    {
+        var reviewer = new Reviewer
+        {
+            Id = Guid.NewGuid(),
+            Active = true,
+            Name = "Richard Eodice",
+            Created = DateTime.Now,
+            Updated = null,
+        };
+
+        try
+        {
+            _repository.Reviewers.Add(reviewer);
+
+            _repository.SaveChanges();
+
+            return Created("Created Reviewer: ", reviewer.Id);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
+
+    }
+}
