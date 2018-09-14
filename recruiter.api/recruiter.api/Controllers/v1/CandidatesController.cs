59f3f9516679dfc145ab7b38263eae56673ef25a
Rich Eodice <rich@eodice.com>
2018-09-14 10:52:16 -0400

    v1 of CandidatesController done.

--- recruiter.api/recruiter.api/Controllers/v1/CandidatesController.cs
+++ recruiter.api/recruiter.api/Controllers/v1/CandidatesController.cs
@@ -15,6 +15,91 @@ public class CandidatesController : ControllerBase
         _repository = repository;
     }
 
+    // Get all candidates, supports paging
+    [HttpGet]
+    [Route("api/v1/[controller]")]
+    public ActionResult GetAll(int take = 25, int skip = 0)
+    {
+        var query = _repository.Candidates
+            .Take(take)
+            .Skip(skip);
+
+        var results = query.ToList();
+
+        return Ok(results);
+    }
+
+    // Get one candidate from id
+    [HttpGet]
+    [Route("api/v1/[controller]/{id}")]
+    public ActionResult GetOne(Guid id)
+    {
+        var result = _repository.Candidates
+            .Find(id);
+
+        if (result != null)
+            return Ok(result);
+        else
+            return NotFound("Candidate does not exist!");
+    }
+
+    // Delete a candidate from id
+    [HttpDelete]
+    [Route("api/v1/[controller]/{id}")]
+    public ActionResult Delete(Guid id)
+    {
+        var result = _repository.Candidates
+            .Find(id);
+
+        if (result != null)
+        {
+            _repository.Remove(result);
+            _repository.SaveChanges();
+            return Ok(result.Id);
+        }
+        else
+        {
+            return NotFound("Candidate does not exist!");
+        }
+    }
+
+    // Create a candidate
+    [HttpPost]
+    [Route("api/v1/[controller]")]
+    public ActionResult Create([FromBody] Candidate candidate)
+    {
+        var result = _repository.Candidates
+            .Find(candidate.Id);
+
+        if (result == null)
+        {
+            _repository.Add(candidate);
+            _repository.SaveChanges();
+            return Ok(candidate.Id);
+        }
+        else
+        {
+            return BadRequest("Candidate already exists");
+        }
+    }
+
+    // Update a candidate
+    [HttpPut]
+    [Route("api/v1/[controller]")]
+    public ActionResult Update([FromBody] Candidate candidate)
+    {
+        if (_repository.Candidates.Contains(candidate))
+        {
+            _repository.Update(candidate);
+            _repository.SaveChanges();
+            return Ok(candidate.Id);
+        }
+        else
+        {
+            return BadRequest("Candidate does not exist");
+        }
+    }
+
     [HttpGet]
     [Route("api/v1/[controller]/create")]
     public ActionResult CreateCandidate()
@@ -41,23 +126,10 @@ public class CandidatesController : ControllerBase
 
             return Created("Created Candidate: ", candidate.Id);
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             return BadRequest(e);
         }
 
-    }
-
-    [HttpGet]
-    [Route("api/v1/[controller]")]
-    public ActionResult Get(int take = 25, int skip = 0)
-    {
-        var query = _repository.Candidates
-            .Take(take)
-            .Skip(skip);
-
-        var results = query.ToList();
-
-        return Ok(results);
     }
 }
